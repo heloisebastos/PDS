d64a3515ee7004b6f424c8cfaf0ade1f971713fd
heloisebastos <heloisebastos28@hotmail.com>
2024-04-02 15:20:45 -0400

    add mais um grafico

--- diagrama_de_polos_e_zeros.m
+++ diagrama_de_polos_e_zeros.m
@@ -6,10 +6,17 @@ zplane(z,p)
 roots(den)
 
 figure;
-num=[1,2,6];den=[1,3];
+num=[1,2,0,6];den=[1,3,0];
 [z,p,k]=tf2zp(num,den);
 zplane(z,p)
 roots(den)
+
+figure;
+num=[1,0];den=[-1/2,-1/3];
+[z,p,k]=tf2zp(num,den);
+zplane(z,p)
+roots(den)
+
 
 
 
